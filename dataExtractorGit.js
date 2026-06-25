f134bf306ec2db45e0710edbfc848aa3fa4cf77e
sanos-gaming <natatanj@gmail.com>
2026-06-25 22:11:24 +0200

    Update dataExtractorGit.js

--- dataExtractorGit.js
+++ dataExtractorGit.js
@@ -247,7 +247,7 @@ async function SmogThreadImport(url1,tourName,tourRound) {
   debugger
   var formats=[]
   console.log("About to import "+LinkList.size+" replays")
-  intentionnalerror()
+  return
   for (const x of LinkList){try { formats.push(await SingleReplayImport(x,db,"https://www.smogon.com"+url1,id))}catch(err){console.error("Failed for "+x+" from https://www.smogon.com"+url1,err)}}
   db.close()
   formats = [...new Set(formats)]
